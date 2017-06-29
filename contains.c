af6f46f7f81cdc120447eb9fff3e20a275a2a333
Kondo Takeo <takeoverjp@gmail.com>
2017-06-29 22:37:22 +0900

    Impl contains with recursive to reduce calc.

--- contains.c
+++ contains.c
@@ -5,18 +5,21 @@
 bool
 contains (int a, int array[], size_t array_size)
 {
-  u_int i = 0;
-
   if (array == NULL || array_size <= 0)
     {
       //fprintf (stderr, "Invalid argument\n");
       return false;
     }
 
-  for (i = 0; i < array_size; i++)
-    {
-      if (a == array[i])
-	return true;
-    }
-  return false;
+  if (array_size == 1)
+    return (a == array[0]) ? true : false;
+
+  size_t l_size = array_size / 2;
+  size_t r_size = array_size - l_size;
+  if (a == array[l_size])
+    return true;
+  else if (a < array[l_size])
+    return contains (a, array, l_size);
+  else
+    return contains (a, array + l_size, r_size);
 }
